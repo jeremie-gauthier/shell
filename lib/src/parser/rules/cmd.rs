8b8882471bc00911009d41f16793861f0a94af58
Jeremie <jeremie@allisone.ai>
2021-12-05 22:24:11 +0100

    feat: parse cmd with args

--- lib/src/parser/rules/cmd.rs
+++ lib/src/parser/rules/cmd.rs
@@ -1,5 +1,6 @@
 use crate::parser::rules::RuleArg;
 use crate::parser::{BTNode, Parser, ParserError};
+use crate::TokenType;
 
 pub trait RuleCmd {
     fn cmd(&mut self) -> Result<BTNode, ParserError>;
@@ -7,7 +8,14 @@ pub trait RuleCmd {
 
 impl RuleCmd for Parser {
     fn cmd(&mut self) -> Result<BTNode, ParserError> {
-        let node = self.arg()?;
-        Ok(node)
+        let mut node = self.arg()?;
+
+        if self.current_token.e_token_type == TokenType::Word {
+            let next_arg = self.cmd()?;
+            node.right = Some(Box::new(next_arg));
+            Ok(node)
+        } else {
+            Ok(node)
+        }
     }
 }
