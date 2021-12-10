98f2259c8577fedcaf0938b26383538bbabfb8b0
Jeremie <jeremie@allisone.ai>
2021-12-10 11:51:13 +0100

    refactor: parser rule cmd branching

--- lib/src/parser/rules/cmd.rs
+++ lib/src/parser/rules/cmd.rs
@@ -10,12 +10,10 @@ impl RuleCmd for Parser {
     fn cmd(&mut self) -> Result<BTNode, ParserError> {
         let mut node = self.arg()?;
 
-        if self.current_token.e_token_type == TokenType::Word {
+		if self.current_token.e_token_type == TokenType::Word {
             let next_arg = self.cmd()?;
             node.right = Some(Box::new(next_arg));
-            Ok(node)
-        } else {
-            Ok(node)
         }
+        Ok(node)
     }
 }
