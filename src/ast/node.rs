bc158dcb900c52e219b69f2b32978eb0a6e9040c
Jeremie <jeremie@allisone.ai>
2021-12-19 19:16:35 +0100

    refactor: idiomatic combinator

---
+++ src/ast/node.rs
@@ -0,0 +1,20 @@
+use crate::lexer::Token;
+
+#[derive(Debug, PartialEq)]
+pub struct Node {
+	token: Token,
+	pub left: Option<Box<Node>>,
+	pub right: Option<Box<Node>>,
+}
+
+impl Node {
+	pub fn new(token: Token, left: Option<Node>, right: Option<Node>) -> Self {
+		let get_node = |node: Node| Box::new(node);
+
+		Node {
+			token,
+			left: left.map(get_node),
+			right: right.map(get_node),
+		}
+	}
+}
