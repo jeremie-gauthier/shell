fa64bdcd51cde034f401c41b1a4562a33e9ee121
Jeremie <jeremie@allisone.ai>
2021-12-19 20:08:34 +0100

    refacto: remove unused tokens

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,14 +1,12 @@
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum TokenType {
-	Whitespace = 0, // space
-	Word,           // alnum
-	Operator,       // generic operator type for lexer
-	Redirection,    // < << > >> &>
-	Pipe,           // |
-	Separator,      // ;
-	Logical,        // && ||
-	Unknown,        // ???
-	End,            // EOF
+	Word = 0,    // alnum
+	Redirection, // < << > >> &>
+	Pipe,        // |
+	Separator,   // ;
+	Logical,     // && ||
+	Unknown,     // ???
+	End,         // EOF
 }
 
 impl Default for TokenType {
