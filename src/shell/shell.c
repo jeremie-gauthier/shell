610ccbc85cdaf0519280a1831fd44c6a1dec548f
jergauth <jeremiegthr@gmail.com>
2022-09-09 20:07:50 +0200

    refactor: rm external lib for shell

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -1,34 +1,12 @@
 #include <stddef.h>
-#include <editline/readline.h>
 // #include <stdio.h>
-#include "shell.h"
 #include "interpreter.h"
-
-static void initialize_readline()
-{
-	using_history();
-	rl_bind_key('\t', rl_complete);
-	rl_attempted_completion_function = (CPPFunction *)command_completion;
-}
-
-static const char *rl_gets(void)
-{
-	const char *const line_read = readline("> ");
-
-	if (line_read && *line_read)
-		add_history(line_read);
-
-	return line_read;
-}
+#include "shell.h"
 
 int shell(void)
 {
-	initialize_readline();
 	while (1)
 	{
-		const char *line_read = rl_gets();
-		interpreter(line_read);
 	}
 	return 0;
 }
-
