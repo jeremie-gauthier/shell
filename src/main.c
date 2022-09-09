3a171a40db18cd5d30840e55c5ee9f4369b0658a
jergauth <jeremiegthr@gmail.com>
2022-09-09 20:08:34 +0200

    refactor: main file for testing

--- src/main.c
+++ src/main.c
@@ -1,44 +1,44 @@
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "token.h"
 #include "lexer.h"
 #include "parser.h"
 #include "shell.h"
+#include "token.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
 
-	shell();
+	// shell();
 
-	// printf("__LEXER__\n");
-	// const t_lexer input_lexer = lexer(argv[1]);
-	// if (!input_lexer.tokens)
-	// {
-	// 	printf("Error: lexer failed\n");
-	// 	return EXIT_FAILURE;
-	// }
+	printf("__LEXER__\n");
+	const t_lexer input_lexer = lexer(argv[1]);
+	if (!input_lexer.tokens)
+	{
+		printf("Error: lexer failed\n");
+		return EXIT_FAILURE;
+	}
 
-	// if (input_lexer.has_error)
-	// {
-	// 	fprintf(stderr, "42sh: grammar error near `%c'\n", input_lexer.input[input_lexer.input_idx]);
-	// 	free_lexer((t_lexer *)&input_lexer);
-	// 	return EXIT_FAILURE;
-	// }
-	// print_tokens(input_lexer.tokens);
+	if (input_lexer.has_error)
+	{
+		fprintf(stderr, "42sh: grammar error near `%c'\n", input_lexer.input[input_lexer.input_idx]);
+		free_lexer((t_lexer *)&input_lexer);
+		return EXIT_FAILURE;
+	}
+	print_tokens(input_lexer.tokens);
 
-	// printf("\n__PARSER__\n");
-	// const t_btree *ast = parse(&input_lexer);
-	// if (!ast)
-	// {
-	// 	printf("Error: parser failed\n");
-	// 	return EXIT_FAILURE;
-	// }
+	printf("\n__PARSER__\n");
+	const t_btree *ast = parse(&input_lexer);
+	if (!ast)
+	{
+		printf("Error: parser failed\n");
+		return EXIT_FAILURE;
+	}
 
-	// print_btree((t_btree *)ast);
+	print_btree((t_btree *)ast);
 
-	// free_lexer((t_lexer *)&input_lexer);
+	free_lexer((t_lexer *)&input_lexer);
 	return EXIT_SUCCESS;
 }
