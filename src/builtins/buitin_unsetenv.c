c683058fff2e19a5fdb2c86328a52c115b95de1d
jergauth <jeremiegthr@gmail.com>
2022-10-30 07:56:20 +0100

    fix(blt): memleaks on unsetenv

--- src/builtins/buitin_unsetenv.c
+++ src/builtins/buitin_unsetenv.c
@@ -13,6 +13,7 @@ int builtin_unsetenv(t_shell *const shell, const t_cmd command)
 	const char *env_var = command.argv[1];
 	const size_t idx = env_get_idx(shell->env, env_var);
 	const size_t bytes_to_copy = sizeof(char *) * ft_arr_len((const char **)&shell->env[idx]);
+	ft_strdel(&shell->env[idx]);
 	ft_memcpy(&shell->env[idx], &shell->env[idx + 1], bytes_to_copy);
 	ht_delete(shell->cache.global, env_var);
 	return EXIT_SUCCESS;
