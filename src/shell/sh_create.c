c672df1063b427fbd9fba94f4755b3e5b0b02f66
jergauth <jeremiegthr@gmail.com>
2022-10-30 07:45:36 +0100

    refactor: shell to malloc the env

--- src/shell/sh_create.c
+++ src/shell/sh_create.c
@@ -1,5 +1,7 @@
 #include "env.h"
+#include "lib_arr.h"
 #include "lib_ht.h"
+#include "lib_str.h"
 #include "shell.h"
 #include <stdlib.h>
 
@@ -14,17 +16,42 @@ static void init_cache(t_cache *cache)
 	}
 }
 
+static char **init_env(char **env)
+{
+	const size_t size = ft_arr_len((char const **)env);
+
+	char **dup_env = malloc(sizeof(char *) * (size + 1));
+	if (!dup_env)
+		return NULL;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		if (!(dup_env[i] = ft_strdup(env[i])))
+		{
+			ft_arr_free(dup_env, i);
+			return NULL;
+		}
+	}
+	dup_env[size] = NULL;
+
+	return dup_env;
+}
+
 t_shell sh_create(char **env)
 {
 	t_shell shell;
 	init_cache(&shell.cache);
-	if (!(load_env_to_cache(shell.cache.global, env)))
+	if (!(shell.env = init_env(env)))
+	{
+		sh_free(shell);
+		exit(EXIT_FAILURE);
+	}
+	if (!(load_env_to_cache(shell.cache.global, shell.env)))
 	{
 		sh_free(shell);
 		exit(EXIT_FAILURE);
 	}
 	shell.status = STOPPED;
-	shell.env = env;
 	shell.last_exit_status = 0;
 	return shell;
 }
