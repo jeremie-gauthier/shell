b19b85f0a334a8c0db50f01d0f4993f4ad78dc43
jergauth <jeremiegthr@gmail.com>
2022-10-31 08:04:05 +0100

    refactor: env util fn name

--- includes/env.h
+++ includes/env.h
@@ -4,7 +4,7 @@
 #include "lib_ht.h"
 #include <stdbool.h>
 
-char *env_get_value(char **env, const char *const key);
+char *env_get(char **env, const char *const key);
 size_t env_get_idx(char **env, const char *const key);
 bool load_env_to_cache(t_ht *cache, char **env);
 bool env_var_name_validate(const char *var);
