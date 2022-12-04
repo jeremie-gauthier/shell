8d6d39662019efe2d794d37da2559534c0f8e837
jergauth <jeremiegthr@gmail.com>
2022-12-04 09:50:21 +0100

    feat(lib): add fn to create arr from size

--- includes/lib_arr.h
+++ includes/lib_arr.h
@@ -7,5 +7,6 @@ void ft_print_arr(const char *const *const arr);
 void ft_arr_free(char **arr, size_t size);
 size_t ft_arr_len(const char *const *const arr);
 char **ft_arr_append(char **arr, const char *const item);
+char **ft_arr_new(size_t size);
 
 #endif
---
+++ lib/arr/ft_arr_new.c
@@ -0,0 +1,13 @@
+#include "lib_mem.h"
+#include <stddef.h>
+
+char **ft_arr_new(size_t size)
+{
+	char **arr = malloc(sizeof(*arr) * (size + 1));
+	if (!arr)
+		return NULL;
+
+	ft_bzero(arr, size + 1);
+
+	return arr;
+}
