0775ae21804d1194d76b901b9008809c3708a0bf
jergauth <jeremiegthr@gmail.com>
2022-12-04 08:40:36 +0100

    refactor: token struct with composite enums

--- includes/token.h
+++ includes/token.h
@@ -16,9 +16,50 @@ enum e_token_type
 	End,
 };
 
+// locate a substring in a string
+typedef struct s_loc
+{
+	const size_t start;
+	const size_t end;
+} t_loc;
+
+// token for shell expansion parameters
+typedef struct s_expansion_token
+{
+	const t_loc loc;
+	const char *parameter;
+} t_expansion_token;
+
+// token for word terminal
+typedef struct s_word_token
+{
+	const char *text;
+	const t_expansion_token *expansion;
+} t_word_token;
+
+// token for command rule
+typedef struct s_command_token
+{
+	const t_word_token name;
+	const t_word_token *suffix;
+} t_command_token;
+
+// token for pipeline rule
+typedef struct s_pipeline_token
+{
+	const t_command_token *commands;
+} t_pipeline_token;
+
+// generic token used in the AST
 typedef struct s_token
 {
-	const char *value;
+	union
+	{
+		t_word_token word;
+		t_expansion_token expansion;
+		t_command_token command;
+		t_pipeline_token pipeline;
+	};
 	enum e_token_type type;
 } t_token;
 
