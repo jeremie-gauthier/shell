bf62d283ca6197b0607be627f8f1510146b44471
jergauth <jeremiegthr@gmail.com>
2022-12-18 16:32:07 +0100

    fix: resize vector segv

--- lib/vec/vec_push.c
+++ lib/vec/vec_push.c
@@ -2,22 +2,27 @@
 #include "lib_vec.h"
 #include <stdlib.h>
 
+#include "token.h"
+#include <stdio.h>
+#include <string.h>
+
+static void vec_resize(t_vec *vec, size_t size)
+{
+	void **items = malloc(sizeof(*items) * size);
+	if (!items)
+		return;
+	ft_memcpy(items, vec->items, sizeof(*items) * vec->size);
+	ft_memdel(vec->items);
+	vec->items = items;
+	vec->capacity = size;
+}
+
 /// @brief Append an item to the vector
 /// @param vec The vector to append the item to
 /// @param item The item to append to the vector
 void vec_push(t_vec *vec, void *item)
 {
 	if (vec->size <= vec->capacity)
-	{
-		vec->items = ft_realloc(vec->items, vec->capacity * 2);
-		if (!vec->items)
-		{
-			vec->capacity = 0;
-			vec->size = 0;
-			return;
-		}
-		vec->capacity *= 2;
-	}
-
+		vec_resize(vec, vec->capacity * 2);
 	vec->items[vec->size++] = item;
 }
