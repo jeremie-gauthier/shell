1e6dd1f0274564191ba363e42288cf1a40411075
jergauth <jeremiegthr@gmail.com>
2022-12-21 11:24:08 +0100

    fix: free tokens & vecs

--- src/ast/ast_free.c
+++ src/ast/ast_free.c
@@ -4,6 +4,28 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+static void free_expansion(void *expansion)
+{
+	t_expansion_token *token = expansion;
+	ft_strdel(&token->parameter);
+	ft_memdel((void **)&token);
+}
+
+static void free_word(void *word)
+{
+	t_word_token *token = word;
+	ft_strdel(&token->text);
+	vec_free(token->param_expansions, free_expansion);
+	ft_memdel((void **)&word);
+}
+
+static void free_cmd(t_command_token *cmd)
+{
+	free_word(cmd->name);
+	vec_free(cmd->suffix, free_word);
+	ft_memdel((void **)&cmd);
+}
+
 void ast_free(t_ast *ast)
 {
 	if (ast == NULL)
@@ -11,7 +33,10 @@ void ast_free(t_ast *ast)
 
 	ast_free(ast->left);
 	ast_free(ast->right);
-	// ft_strdel((char **)&ast->token.data);
+	if (ast->token.type == Command)
+		free_cmd(ast->token.command);
+	else if (ast->token.type == Word)
+		free_word(ast->token.word);
 	ft_memdel((void **)&ast);
 	ast = NULL;
 }
